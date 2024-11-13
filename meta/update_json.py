de1c4e355ccd977e9acbf82b1b74da0b336045c6
lilellia <pixielf.0@gmail.com>
2024-11-13 16:44:10 -0500

    add SleepyKittyASMR's fill of autumn picnic

--- meta/update_json.py
+++ meta/update_json.py
@@ -1,3 +1,5 @@
+from argparse import ArgumentParser
+from collections.abc import Iterable, Mapping
 import json
 from pathlib import Path
 from typing import Any
@@ -12,11 +14,8 @@ def load_data(datafile: Path) -> list[dict[str, Any]]:
     result: list[dict[str, Any]] = []
 
     for script in data["scripts"]:
-        # remove any script which isn't published
-        if script.get("published") is None:
-            continue
-
-        script["published"] = script["published"].strftime("%Y-%m-%d")
+        if script.get("published") is not None:
+            script["published"] = script["published"].strftime("%Y-%m-%d")
 
         if dt := script.get("finished"):
             script["finished"] = dt.strftime("%Y-%m-%d")
@@ -37,14 +36,30 @@ def load_data(datafile: Path) -> list[dict[str, Any]]:
     return result
 
 
+def remove_unpublished(data: Iterable[Mapping[str, Any]]) -> list[dict[str, Any]]:
+    return [dict(x) for x in data if x.get("published") is not None]
+
+
 def main():
-    in_file = Path(__file__).parent.parent / "script-data.yaml"
-    out_file = Path(__file__).parent.parent / "script-data.json"
+    parser = ArgumentParser()
+    parser.add_argument("-i", "-y", "--in-file", type=Path, help="the source .yaml file to read", required=True)
+    parser.add_argument("-o", "--out-file", type=Path, help="the output .json file", required=True)
+    parser.add_argument("-p", "--private", type=Path, help="the output path for the private .json file")
+    args = parser.parse_args()
 
-    data = {"scripts": load_data(in_file)}
+    # read the source data
+    all_scripts = load_data(args.in_file)
+    public_scripts = remove_unpublished(all_scripts)
 
-    with open(out_file, "w", encoding="utf-8") as f:
-        json.dump(data, f, indent=4)
+    # output the data to file
+    with open(args.out_file, "w", encoding="utf-8") as f:
+        json.dump({"scripts": public_scripts}, f, indent=4)
+
+    if args.private is None:
+        args.private = args.out_file.with_stem(f"{args.out_file.stem}-private")
+
+    with open(args.private, "w", encoding="utf-8") as f:
+        json.dump({"scripts": all_scripts}, f, indent=4)
 
 
 if __name__ == "__main__":
